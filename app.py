78599cee79604c4d24174a8ceea4a510cda9ea92
theNafsters <48960696+theNafsters@users.noreply.github.com>
2022-03-11 12:23:24 -0500

    FLAP-188
    uncommented unicorn & commented out demo.db

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from library_service.dataaccess.sql.postgres_sql_store import PostgresStore
 
 # Runs the app in development mode
 def main() -> None:
-    demo_db()
-    # uvicorn.run("library_service.main:app", host="localhost", port=8080, reload=True)
+ #   demo_db()
+    uvicorn.run("library_service.main:app", host="localhost", port=8080, reload=True)
 
 
-def demo_db() -> None:
-    """A demo of how to utilize the database"""
-    pgstore = PostgresStore()
-    pgstore.save_file("jeff1", "somefile1.txt")
-    pgstore.save_files("jeff1", "somefile1.txt", "file2.text")
-    f = pgstore.load_file("jeff", "somefile.txt")
-    print(f)
+#def demo_db() -> None:
+#    """A demo of how to utilize the database"""
+#    pgstore = PostgresStore()
+#    pgstore.save_file("jeff1", "somefile1.txt")
+#    pgstore.save_files("jeff1", "somefile1.txt", "file2.text")
+#    f = pgstore.load_file("jeff", "somefile.txt")
+#    print(f)
 
 
 if __name__ == "__main__":
